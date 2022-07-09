42ac85611d672b3c78cfc6509a75512f075b3d05
Santiago Salgado <jssalgadog@gmail.com>
2022-07-09 10:53:00 -0500

    create_schemas

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
+# Python
+from typing import Optional
+from datetime import datetime
+
 # uvicorn
 import uvicorn
 
+# pydantic
+from pydantic import BaseModel
+
 # Fast API
 from fastapi import FastAPI 
+
+# User Model
+class User(BaseModel):
+    id: int
+    first_name: str
+    last_name: str
+    address: Optional[str]
+    phone_number: int
+    created_at: datetime = datetime.now()
 
 
 app = FastAPI()
@@ -11,6 +27,12 @@ app = FastAPI()
 def ruta1():
     return {"message": "Bienvenido a tu primera api"}
 
+@app.post('/ruta2')
+def ruta2(user: User):
+    print(user)
+    print(user.dict())
+    return True
+
 # Other way to run the server
 if __name__ == "__main__":
-    uvicorn.run("main.app", port = 8000)
+    uvicorn.run("main.app", port = 8000, reload=True)
