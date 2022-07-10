3cd97cd6e5980fc9005bd1751bd46ad075082287
Santiago Salgado <jssalgadog@gmail.com>
2022-07-10 16:48:03 -0500

    create_environment_variables

--- app/db/database.py
+++ app/db/database.py
@@ -1,8 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from core.config import settings
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:123456@localhost:5432/fastapidatabase"
+# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:123456@localhost:5432/fastapidatabase"
+
+SQLALCHEMY_DATABASE_URL = settings.SQLALCHEMY_DATABASE_URL
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 Base = declarative_base()
