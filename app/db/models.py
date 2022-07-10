1a82d9f81bacc68ba9045d86f67f069fc6cad3eb
Santiago Salgado <jssalgadog@gmail.com>
2022-07-10 09:43:53 -0500

    create_sales_model

--- app/db/models.py
+++ app/db/models.py
@@ -2,22 +2,14 @@ from datetime import datetime
 
 from app.db.database import Base
 from sqlalchemy import Column, Integer, String, Boolean, DateTime
-
-"""
-class User(BaseModel):
-    id: int
-    first_name: str
-    last_name: str
-    address: Optional[str]
-    phone_number: int
-    created_at: datetime = datetime.now()
-
-
-"""
+from sqlalchemy.schema import ForeignKey
+from sqlalchemy.orm import relationship
 
 class User(Base):
     __tablename__ = "user"
     id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String)
+    password = Column(String)
     first_name = Column(String)
     last_name = Column(String)
     address = Column(String)
@@ -25,3 +17,11 @@ class User(Base):
     email = Column(String)
     created_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
     status = Column(Boolean)
+    sale = relationship("Sale", backref="user", cascade="delete,merge")
+
+class Sale(Base):
+    __tablename__ = "sale"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey("user.id", ondelete="CASCADE"))
+    sale = Column(Integer)
+    sale_product = Column(Integer)
