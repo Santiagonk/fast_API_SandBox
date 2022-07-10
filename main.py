9fcb0c73366e24062343032dd3c9b74dfed00d85
Santiago Salgado <jssalgadog@gmail.com>
2022-07-10 17:16:41 -0500

    migrate_models_with_alembic

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ from fastapi import FastAPI
 from app.routers import user
 from app.db.database import Base, engine
 
-def create_tables():
-    Base.metadata.create_all(bind=engine)
-create_tables()
+# def create_tables():
+#     Base.metadata.create_all(bind=engine)
+# create_tables()
 
 app = FastAPI()
 app.include_router(user.router)
